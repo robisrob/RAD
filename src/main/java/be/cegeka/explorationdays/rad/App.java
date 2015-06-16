858abeb4df7e02d8e3b82eaa043a481cb2f9ab3f
rRob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-06-16 11:21:10 +0200

    Allowed Cross Origin Access

--- src/main/java/be/cegeka/explorationdays/rad/App.java
+++ src/main/java/be/cegeka/explorationdays/rad/App.java
@@ -10,9 +10,14 @@ import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class App extends Application<MessagewallConfiguration>
 {
@@ -32,6 +37,13 @@ public class App extends Application<MessagewallConfiguration>
         CachingAuthenticator<BasicCredentials, Boolean> authenticator = new CachingAuthenticator<>(environment.metrics(), new MessageAuthenticator(jdbi), configuration.getAuthenticationCachePolicy());
         environment.jersey().register(new MessageResource(jdbi, environment.getValidator()));
         environment.jersey().register(AuthFactory.binder(new BasicAuthFactory<>(authenticator, "Webservice Authentication", Boolean.class)));
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
 
         environment.healthChecks().register("Database health check", new DatabaseHealthCheck(jdbi));
     }
